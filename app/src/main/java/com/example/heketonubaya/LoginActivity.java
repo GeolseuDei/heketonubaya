2127d45ecafeec9582e901c3b7cf8895df11826c
Erwin Kurniawan A <erwinkurniawanadi@gmail.com>
2018-08-29 12:52:02 +0700

    splash n login act

---
+++ app/src/main/java/com/example/heketonubaya/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.heketonubaya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText etUsername, etPassword;
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        Button btnLogin = findViewById(R.id.btn_login);
+        
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/heketonubaya/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.heketonubaya;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new CountDownTimer(3000,1000) {
+
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        }.start();
+    }
+}
