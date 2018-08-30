52fdd8cad312241a23bc0ee619c9c93107126069
Tommy <tommyrachmadiono@gmail.com>
2018-08-30 18:25:35 +0700

    register

---
+++ belajar_json/register.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hackathon_ubaya";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+} 
+
+$username = mysqli_real_escape_string($conn, $_POST['username']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+$role = mysqli_real_escape_string($conn, $_POST['role']);
+$status = mysqli_real_escape_string($conn, $_POST['status']);
+
+$sql = "INSERT INTO user (username, password, role, status)
+VALUES ('$username', '$password', '$role@', '$status')";
+
+if ($conn->query($sql) === TRUE) {
+	echo "New record created successfully";
+} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
